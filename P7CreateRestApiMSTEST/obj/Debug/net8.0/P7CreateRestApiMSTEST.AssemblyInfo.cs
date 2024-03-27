c94ab412452d6f3b45ffee2c7d2847d4834cf84e
Thomas COLLET <thomas.collet@axa.fr>
2024-03-27 08:21:05 +0100

    merge de la suppression d'area

--- P7CreateRestApiMSTEST/obj/Debug/net8.0/P7CreateRestApiMSTEST.AssemblyInfo.cs
+++ P7CreateRestApiMSTEST/obj/Debug/net8.0/P7CreateRestApiMSTEST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e193b0b50a47804728c4bc558e5543601ff552c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b48ba4e79cafcfac1966c854e885dc019db07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
