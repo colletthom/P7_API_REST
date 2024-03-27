43e216b6cab88cfa3923ca72392ded9580e25c66
Thomas COLLET <thomas.collet@axa.fr>
2024-03-27 08:07:09 +0100

    La suppresison du Area fonctionne

--- P7CreateRestApiMSTEST/obj/Debug/net8.0/P7CreateRestApiMSTEST.AssemblyInfo.cs
+++ P7CreateRestApiMSTEST/obj/Debug/net8.0/P7CreateRestApiMSTEST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3315f579df9f017958b83babbe8b5aa2ce494fbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf686687dd436bb50aa3d393d67b5b32e07d8b74")]
 [assembly: System.Reflection.AssemblyProductAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P7CreateRestApiMSTEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
